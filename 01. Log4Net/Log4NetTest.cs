f07654a3dd30672b2efcddaeec2247656bbb4031
nader-dab <nader.dabour87@abv.bg>
2013-05-17 22:52:19 +0300

    My Development Tools Homework

---
+++ 01. Log4Net/Log4NetTest.cs
@@ -0,0 +1,38 @@
+﻿// ********************************
+// <copyright file="Log4NetTest.cs" company="Telerik Academy">
+// Copyright (c) 2013 Telerik Academy. All rights reserved.
+// </copyright>
+//
+// ********************************
+namespace _01.Log4Net
+{
+    using System;
+    using log4net;
+    using log4net.Config;
+
+    public class Log4NetTest
+    {
+        private static readonly ILog Debug =
+            LogManager.GetLogger("Debug");
+
+        private static readonly ILog Exceptions =
+            LogManager.GetLogger("Exception");
+
+        public static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+            try
+            {
+                Debug.Info("Program Start");
+                Console.WriteLine("Please input a number");
+                int numner = int.Parse(Console.ReadLine());
+                Console.WriteLine("Your input was a the number {0}", numner);
+                Debug.Info("Program End");
+            }
+            catch (Exception ex)
+            {
+                Exceptions.Error(ex.Message);
+            }
+        }
+    }
+}
---
+++ 02. T4Template/T4CodeGeneration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class T4Test
+{
+	static void Main()
+	{
+		Console.WriteLine("Please enter a digit");
+		int digit = int.Parse(Console.ReadLine());
+		switch(digit)
+		{
+			case 0 : Console.WriteLine("The ASCI code of {0} is {1}.", 0, (int)'0'); break;
+			case 1 : Console.WriteLine("The ASCI code of {0} is {1}.", 1, (int)'1'); break;
+			case 2 : Console.WriteLine("The ASCI code of {0} is {1}.", 2, (int)'2'); break;
+			case 3 : Console.WriteLine("The ASCI code of {0} is {1}.", 3, (int)'3'); break;
+			case 4 : Console.WriteLine("The ASCI code of {0} is {1}.", 4, (int)'4'); break;
+			case 5 : Console.WriteLine("The ASCI code of {0} is {1}.", 5, (int)'5'); break;
+			case 6 : Console.WriteLine("The ASCI code of {0} is {1}.", 6, (int)'6'); break;
+			case 7 : Console.WriteLine("The ASCI code of {0} is {1}.", 7, (int)'7'); break;
+			case 8 : Console.WriteLine("The ASCI code of {0} is {1}.", 8, (int)'8'); break;
+			case 9 : Console.WriteLine("The ASCI code of {0} is {1}.", 9, (int)'9'); break;
+			default: Console.WriteLine("Invalid Digit"); break;
+		}
+	}
+}
